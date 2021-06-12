2de611c935e17f80dbc3b0f7dafbd740df4de07b
Lukas TAMAYO <luknight@hotmail.fr>
2021-06-12 11:55:46 +0200

    [Controls]<Basic>Added a basic character to the project

---
+++ Assets/Scripts/Character/TopDownCtrl.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDownCtrl : MonoBehaviour
+{
+    Rigidbody2D myRigidbody = null;
+    Animator myAnimator = null;
+    SpriteRenderer mySpriteRenderer = null;
+    SpriteRenderer myChildSpriteRenderer = null;
+
+    public float speed = 1f;
+
+
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
+        myChildSpriteRenderer = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        //Idle
+        if (horizontal == 0f && vertical == 0f)
+		{
+            mySpriteRenderer.flipX = false;
+            myChildSpriteRenderer.flipX = false;
+            myAnimator.Play("Idle");
+		}
+        else if (vertical <= -1f)
+		{
+            mySpriteRenderer.flipX = false;
+            myChildSpriteRenderer.flipX = false;
+            myAnimator.Play("Down");
+        }
+        else if (vertical >= 1f)
+		{
+            mySpriteRenderer.flipX = false;
+            myChildSpriteRenderer.flipX = false;
+            myAnimator.Play("Up");
+        }
+        else if (horizontal <= -1f)
+		{
+            myAnimator.Play("Horizontal");
+            mySpriteRenderer.flipX = true;
+            myChildSpriteRenderer.flipX = true;
+        }
+        else if (horizontal >= 1f)
+		{
+            myAnimator.Play("Horizontal");
+            mySpriteRenderer.flipX = false;
+            myChildSpriteRenderer.flipX = false;
+        }
+
+        myRigidbody.velocity = new Vector2(horizontal, vertical).normalized * speed;
+    }
+}
---
+++ Assets/Scripts/Tools/TimerBasic.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerBasic : MonoBehaviour
+{
+    //Mon temps d'attente
+    public float delay = 5f;
+    //Mon chronometre
+    float chrono = 0f;
+
+    //Se fait une fois quand on appuie sur le bouton play
+    void Start()
+    {
+        //On met le temps entre chaque action 
+        chrono = delay;
+    }
+
+    //Se fait tout le temps
+    void Update()
+    {
+        chrono -= Time.deltaTime;
+        //chrono = chrono - Time.deltaTime;
+        if (chrono <= 0f)
+		{
+            GetComponent<SpriteRenderer>().color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+            chrono = delay;
+		}
+    }
+}
